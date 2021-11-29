2e353c399ddd204262c59ddcd03f7d002390c216
Temchar <89862534+Temchar@users.noreply.github.com>
2021-11-29 09:57:42 -0500

    Add files via upload

---
+++ portfolio/Stats project/Stats JS.js
@@ -0,0 +1,218 @@
+/**You have to list the individual arrays before the complete one, and they have to be variables, not strings
+when you list them, Also this is a list of all the songs showing genre, title, and then artist**/
+const song1 = ['alternative', 'amsterdam', 'imagine dragons'];
+const song2 = ['pop', 'traitor', 'olivia rodrigo'];
+const song3 = ['pop', "1-800-DATEME", 'mxmtoon'];
+const song4 = ['edm', "a cyber's world?", 'toby fox'];
+const song5 = ['rock', 'all I want', 'a day to remember'];
+const song6 = ['pop', 'chime', 'ai otsuka'];
+const song7 = ['alternative', 'way less sad', 'ajr'];
+const song8 = ['pop', '100 bad days', 'ajr'];
+const song9 = ['pop', "bang!", 'ajr'];
+const song10 = ['alternative', 'die for the government', "anti-flag"];
+const song11 = ['alternative', 'do I wanna know', 'arctic monkeys'];
+const song12 = ['rock', "why'd you only call me when youre high", 'arctic monkeys'];
+const song13 = ['pop', 'churchyard', 'aurora'];
+const song14 = ['hip hop', "no new friends!", 'autumn'];
+const song15 = ['rock', 'hail to the king', 'avenged sevenfold'];
+const song16 = ['edm', 'big shot', 'toby fox'];
+const song17 = ['alternative', 'I wanna be a punk', 'blanks 77'];
+const song18 = ['rock', 'livin on a prayer', 'bon jovi'];
+const song19 = ['country', 'mud on tires', 'brad paisley'];
+const song20 = ['rock', 'halo theme gungir mix', 'brian trifon'];
+const song21 = ['edm', 'checker dance', 'toby fox'];
+const song22 = ['country', 'big big plans', 'chris lane'];
+const song23 = ['rock', 'black eyes blue', 'corey taylor'];
+const song24 = ['country', 'moss', 'cosmo sheldrake'];
+const song25 = ['country', 'pliocene', 'cosmo sheldrake'];
+const song26 = ['hip hop', 'crank that', 'souja boy tell em'];
+const song27 = ['hip hop', 'streetcar', 'daniel caesar'];
+const song28 = ['country', 'tennessee river run', 'darryl worley'];
+const song29 = ['rock', 'pour some sugar on me', 'def leppard'];
+const song30 = ['hip hop', 'streets', 'doja cat'];
+const song31 = ['hip hop', 'tsu', 'drake'];
+const song32 = ['edm', 'satin doll', 'duke ellington'];
+const song33 = ['pop', 'arcade', 'duncan laurence'];
+const song34 = ['rock', 'hotel california', 'eagles'];
+const song35 = ['country', 'love aint', 'eli young band'];
+const song36 = ['hip hop', 'my new love', 'elijah who'];
+const song37 = ['country', 'american rust', 'ernest'];
+const song38 = ['rock', 'final countdown', 'europe'];
+const song39 = ['hip hop', 'family ties', 'baby keem and Kendrick lamar'];
+const song40 = ['country', 'matter of time', 'five finger death punch'];
+const song41 = ['hip hop', 'flashing lights', 'kanye'];
+const song42 = ['hip hop', 'swim good', 'frank ocean'];
+const song43 = ['hip hop', 'get along better', 'drake'];
+const song44 = ['alternative', 'youth', 'glass animals'];
+const song45 = ['pop', 'heatwaves', 'glass animals'];
+const song46 = ['alternative', 'stigmata', 'grandson'];
+const song47 = ['alternative', 'stick up', 'grandson'];
+const song48 = ['alternative', 'basket case', 'green day'];
+const song49 = ['rock', 'welcome to the jungle', 'guns n roses'];
+const song50 = ['alternative', 'creature', "half-alive"];
+const song51 = ['alternative', 'arsonists lullaby', 'hozier'];
+const song52 = ['hip hop', 'it was a good day', 'ice cube'];
+const song53 = ['hip hop', 'imy2', 'drake'];
+const song54 = ['edm', 'its pronounced rules', 'toby fox'];
+const song55 = ['hip hop', 'she knows', 'j cole'];
+const song56 = ['hip hop', 'power trip', 'j cole'];
+const song57 = ['hip hop', 'nobodys perfect', 'j cole'];
+const song58 = ['country', 'nail in my coffin', 'jerry irby'];
+const song59 = ['rock', 'hurts so good', 'john mellencamp'];
+const song60 = ['country', 'buy dirt', 'jordan davis'];
+const song61 = ['hip hop', 'black and white', 'juice wrld'];
+const song62 = ['hip hop', 'wishing well', 'juice wrld'];
+const song63 = ['hip hop', 'money trees', 'kendrick lamar'];
+const song64 = ['alternative', 'she calls me daddy', 'king mala'];
+const song65 = ['hip hop', 'lose', 'ksi'];
+const song66 = ['hip hop', 'montero', 'lil nas x'];
+const song67 = ['hip hop', 'falling down', 'lil peep'];
+const song68 = ['hip hop', 'sanguine paradise', 'lil uzi vert'];
+const song69 = ['hip hop', 'scared of the dark', 'lil wayne'];
+const song70 = ['pop', 'gurenge', 'lisa'];
+const song71 = ['edm', 'fur elise', 'ludwig van beethoven'];
+const song72 = ['edm', 'innocence', 'madeon'];
+const song73 = ['edm', 'silence', "marshmallow/khalid"];
+const song74 = ['rock', 'monster', 'skillet'];
+const song75 = ['alternative', 'hayloft', 'mother mother'];
+const song76 = ['alternative', 'burning pile', 'mother mother'];
+const song77 = ['rock', 'smells like teen spirit', 'nirvana'];
+const song78 = ['hip hop', 'hit the scene', 'nle choppa'];
+const song79 = ['alternative', 'leave it alone', 'nofx'];
+const song80 = ['pop', 'traitor', 'olivia rodrigo'];
+const song81 = ['rock', 'another brick in the wall', 'pink Floyd'];
+const song82 = ['hip hop', 'over', 'playboy carti'];
+const song83 = ['hip hop', 'uh huh', 'quadeca'];
+const song84 = ['hip hop', 'butterflies', 'queen naija'];
+const song85 = ['rock', 'amerika', 'rammstein'];
+const song86 = ['rock', 'ryan and dave', 'rare americans'];
+const song87 = ['rock', 'baggage', 'rare americans'];
+const song88 = ['alternative', 'cats dogs and rats', 'rare americans'];
+const song89 = ['rock', 'keep on loving you', 'reo speedwagon'];
+const song90 = ['country', 'different round here', 'riley green'];
+const song91 = ['country', 'watching you', 'rodney atkins'];
+const song92 = ['rock', 'tom sawyer', 'rush'];
+const song93 = ['alternative', 'sweet tooth', 'scott helman'];
+const song94 = ['hip hop', 'nuketown', 'ski mask the slump god'];
+const song95 = ['rock', 'soloway firth', 'slipknot'];
+const song96 = ['hip hop', 'gin and juice', 'snoop dog'];
+const song97 = ['pop', 'magic in the hamptons', 'social house'];
+const song98 = ['hip hop', 'nobody but you', 'sonder'];
+const song99 = ['hip hop', 'ryd', 'steve lacy'];
+const song100 = ['rock', '82', 'stone sour'];
+const song101 = ['pop', 'Sunday best', 'surfaces'];
+const song102 = ['rock', 'eye of the tiger', 'survivor'];
+const song103 = ['rock', 'genocidal humanoidz', 'system of a down'];
+const song104 = ['pop', 'once in a lifetime', 'talking heads'];
+const song105 = ['pop', 'stupid', 'tate mcrae'];
+const song106 = ['country', 'hello', 'the cat empire'];
+const song107 = ['rock', 'rhode island', 'the front bottoms'];
+const song108 = ['rock', 'wolfman', 'the front bottoms'];
+const song109 = ['rock', 'feel good inc', 'gorillaz'];
+const song110 = ['alternative', 'night ride', 'the growlers'];
+const song111 = ['hip hop', 'shot for me', 'the kid laroi'];
+const song112 = ['alternative', 'prisoner of society', 'the living end'];
+const song113 = ['country', 'on the railroad', 'the longest johns'];
+const song114 = ['country', 'thus always to tyrants', 'the oh hellos'];
+const song115 = ['rock', 'every breath you take', 'the police'];
+const song116 = ['rock', 'blitzkreig bop', 'the ramones'];
+const song117 = ['hip hop', 'die for you', 'the weeknd'];
+const song118 = ['edm', 'the world revolving', 'toby fox'];
+const song119 = ['edm', 'nows your chance to', 'toby fox'];
+const song120 = ['edm', 'faint courage', 'toby fox'];
+const song121 = ['edm', 'vs lancer', 'toby fox'];
+const song122 = ['edm', 'field of hopes and dreams', 'toby fox'];
+const song123 = ['edm', 'rouxls kaard', 'toby fox'];
+const song124 = ['hip hop', 'dark knight dummo', 'trippie redd'];
+const song125 = ['hip hop', "1400/999 freestyle", 'trippie redd'];
+const song126 = ['rock', 'car radio', 'twenty one pilots'];
+const song127 = ['hip hop', 'earfquake', 'tyler the creator'];
+const song128 = ['edm', 'parking lot', 'uamee'];
+const song129 = ['rock', 'jump', 'van halen'];
+const song130 = ['rock', 'alive', 'warbly jets'];
+const song131 = ['hip hop', "sad!", 'xxxtentacion'];
+const song132 = ['hip hop', 'get busy', 'yeat'];
+const song133 = ['alternative', 'letter from ajax', "yu-peng chen"];
+const song134 = ['country', 'toes', 'zac brown'];
+const song135 = ['rock', 'cannibal', 'tally hall'];
+/**This holds all the song arrays so their should be equal odds of all songs except the one repeat, and the other variables keep track of how often a
+genre pops up**/
+const playlist = [song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11, song12, song13, song14, song15, song16, song17, song18,
+song19, song20, song21, song22, song23, song24, song25, song26, song27, song28, song29, song30, song31, song32, song33, song34, song35, song36, song37,
+song38, song39, song40, song41, song42, song43, song44, song45, song46, song47, song48, song49, song50, song51, song52, song53, song54, song55, song56,
+song57, song58, song59, song60, song61, song62, song63, song64, song65, song66, song67, song68, song69, song70, song71, song72, song73, song74, song75,
+song76, song77, song78, song79, song80, song81, song82, song83, song84, song85, song86, song87, song88, song89, song90, song91, song92, song93, song94,
+song95, song96, song97, song98, song99, song100, song101, song102, song103, song104, song105, song106, song107, song108, song109, song110, song111,
+song112, song113, song114, song115, song116, song117, song118, song119, song120, song121, song122, song123, song124, song125, song126, song127, song128,
+song129, song130, song131, song132, song133, song134, song135];
+let rock = 0;
+let edm = 0;
+let pop = 0;
+let country = 0;
+let alternative = 0;
+let hipHop = 0;
+let totalSongs = 0;
+
+/**A functioning shuffle that checks all songs and works even if arrays have different lengths before marking what genre they were**/
+function shuffle() {
+let i = getRandomInt(playlist.length);
+document.getElementById('song').innerHTML = "Song: " + playlist[i][0] + ' - ' + playlist[i][1] + ' by ' + playlist[i][2];
+totalSongs += 1;
+switch(playlist[i][0]) {
+case 'rock':
+rock += 1;
+break;
+case 'edm':
+edm += 1;
+break;
+case 'pop':
+pop += 1;
+break;
+case 'country':
+country += 1;
+break;
+case 'alternative':
+alternative += 1;
+break;
+case 'hip hop':
+hipHop += 1;
+break;
+default:
+break;
+}
+}
+
+/**Track the data all the way to 50, this posts the amount, percent, and fraction of each genre**/
+function stats() {
+document.getElementById('stats').innerHTML = "Total: " + totalSongs
++ "<br/> Alternative: " + alternative + " = " + alternative/totalSongs + " or " + alternative + "/" + totalSongs
++ "<br/> Country: " + country + " = " + country/totalSongs + " or " + country + "/" + totalSongs
++ "<br/> EDM: " + edm + " = " + edm/totalSongs + " or " + edm + "/" + totalSongs
++ "<br/> Hip Hop: " + hipHop + " = " + hipHop/totalSongs + " or " + hipHop + "/" + totalSongs
++ "<br/> Pop: " + pop + " = " + pop/totalSongs + " or " + pop + "/" + totalSongs
++ "<br/> Rock: " + rock + " = " + rock/totalSongs + " or " + rock + "/" + totalSongs;
+}
+
+/**This resets the variables as well as the displayed data**/
+function clearMe() {
+rock = 0;
+edm = 0;
+pop = 0;
+country = 0;
+alternative = 0;
+hipHop = 0;
+totalSongs = 0;
+document.getElementById('song').innerHTML = "Song: ";
+document.getElementById('stats').innerHTML = "Total: 0"
++ "<br/> Alternative: 0 = NaN or 0/0"
++ "<br/> Country: 0 = NaN or 0/0"
++ "<br/> EDM: 0 = NaN or 0/0"
++ "<br/> Hip Hop: 0 = NaN or 0/0"
++ "<br/> Pop: 0 = NaN or 0/0"
++ "<br/> Rock: 0 = NaN or 0/0";
+}
+
+/**A function for getting a random number in a range**/
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
